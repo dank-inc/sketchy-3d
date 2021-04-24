010bba6e7fc72b8fb6cec85622d0c382ef3050e5
Elijah Lucian <elijahlucian@gmail.com>
2021-04-23 18:40:11 -0600

    📦

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
-import {
-  create3dSketch,
-  createParams,
-  load3dSketch,
-  useGeometry,
-  useLight,
-} from "./lib";
-import { useAmbient, useMaterial, useMesh } from "./lib";
+import { createParams, load3dSketch } from './lib'
+import cube from './sketches/cube'
+import outrunmountains from './sketches/outrunmountains'
+import spaceport from './sketches/spaceport'
 
 const params = createParams({
-  containerId: "root",
-  dimensions: [600, 600],
+  containerId: 'root',
+  // dimensions: [600, 600],
   animate: true,
-});
+})
 
-const sketch = create3dSketch(({ scene, camera }) => {
-  scene.add(useAmbient());
-  scene.add(useLight(0xffffff, 0.4, [-1, 1, -1]));
-  const mesh = useMesh(
-    useGeometry("box", [1, 1, 1]),
-    useMaterial("standard", 0x89cad0)
-  );
-  scene.add(mesh);
-
-  return ({ time, dt }) => {
-    mesh.rotation.y += dt;
-    params.renderer.render(scene, camera);
-  };
-});
-
-load3dSketch(sketch, params);
+load3dSketch(cube, params)
---
+++ src/sketches/cube.ts
@@ -0,0 +1,60 @@
+import glslify from 'glslify'
+import { Vector3 } from 'three'
+import {
+  create3dSketch,
+  useAmbient,
+  useGeometry,
+  useLight,
+  useShader,
+  useShaderMesh,
+} from '../lib'
+// import { frag, vert } from './shaders/cube'
+
+export default create3dSketch(({ scene, camera, renderer }) => {
+  scene.add(useAmbient())
+  scene.add(useLight(0xffffff, 0.4, [-1, 1, -1]))
+
+  camera.position.y = 0
+  camera.lookAt(new Vector3())
+
+  const creator = new URLSearchParams(window.location.search).get('creator')
+  const viewer = new URLSearchParams(window.location.search).get('viewer')
+  const addr = 'tz1SLpSREWeSSdHhMTVgt1ygi9pkgeBZFGRG'
+
+  const s = 6
+
+  const mesh = useShaderMesh(
+    useGeometry('box', [s, s, s]),
+    useShader({ frag, vert }, [['time', 0]]),
+  )
+  scene.add(mesh)
+
+  return ({ time, dt }) => {
+    mesh.rotation.y += dt * 0.5
+
+    mesh.material.uniforms.time.value = time
+
+    renderer.render(scene, camera)
+  }
+})
+
+const vert = `
+varying vec2 vUv;
+void main() {
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xyz, 1.0);
+}
+`
+
+const frag = `
+varying vec2 vUv;
+uniform float time;
+void main() {
+
+  float d = distance(sin(time + vUv.x * 4.0), vUv.y);
+
+  float t = mod(time + d * 2.0, 1.0);
+  vec3 fragColor = vec3(t);
+
+  gl_FragColor = vec4(fragColor, 0.1);
+}
+`
