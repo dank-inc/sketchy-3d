54e0eb7f9a78021106c3cd5c182a6a56f38aab7c
Elijah <elijahlucian@gmail.com>
2025-01-05 08:38:56 -0700

    box

--- demo/index.ts
+++ demo/index.ts
@@ -15,18 +15,14 @@ const params = createParams({
   element: document.getElementById('root')!,
   // dimensions: [600, 600],
   animate: true,
-  data: {
-    clicked: false,
-  },
   // background: [0xffffff, 0],
 })
 
 const sketch = create3dSketch(
-  ({ scene, camera, renderer, PI, TAU, container, sin, cos, data }) => {
+  ({ scene, camera, renderer, PI, TAU, container, sin, cos }) => {
     const ambient = useAmbient(0xffffff, 0)
     scene.add(ambient)
 
-    const mouse = new SuperMouse({ element: container, scrollScale: 0.001 })
     const light = useLight(0xffffff, 1, [1, 1, 1])
     scene.add(light)
 
@@ -37,17 +33,31 @@ const sketch = create3dSketch(
 
     light.lookAt(box.position)
 
-    mouse.onScroll = ({ deltaY }) => {
-      box.rotation.y -= deltaY
+    const data = {
+      clicked: false,
     }
 
-    data.clicked = false
+    const mouse = new SuperMouse({ element: container, scrollScale: 0.001 })
+    mouse.onScroll = (e) => {
+      e.stopPropagation()
+
+      const deltaY = e.deltaY
+
+      box.rotation.y -= deltaY
+    }
+    mouse.onClick = (e) => {
+      e.stopPropagation()
+
+      data.clicked = !data.clicked
+      box.rotation.x += PI * 0.25
+    }
 
     return ({ time, dt }) => {
       // box.rotation.y += dt * 0.5
       // box.position.y = 1 + Math.sin(time) * 1
 
       box.rotation.y += mouse.scrollInertia * 0.0001
+      box.rotation.y += dt
 
       if (mouse.scrollInertia >= 0) {
         light.color.set(0xffffff)
